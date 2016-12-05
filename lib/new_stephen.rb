885cb6c0f355d1b9716ff51b3ea2c16cab5e5a35
JJ Asghar <jj@chef.io>
2016-12-05 12:57:24 -0600

    Added original airdate
    - Added an original airdate
    
    Signed-off-by: JJ Asghar <jj@chef.io>

--- lib/new_stephen.rb
+++ lib/new_stephen.rb
@@ -10,7 +10,13 @@ set :root, File.expand_path(Dir.pwd)
 get '/' do
   client = SDClient.new(USERNAME, PASSWORD)
   client.authenticate
-  next_colbert = client.next_episode("33424","EP01906276")['new']
+  new_colbert = client.next_episode("33424","EP01906276")['new']
+  program_id = client.next_episode("33424","EP01906276")['programID']
+  original_airdate_post = client.post("programs", [program_id])
+  original_airdate = JSON.parse(original_airdate_post.body)[0]['originalAirDate']
 
-  erb :index, :locals => {:next_colbert => next_colbert }
+  erb :index, :locals => {
+        :new_colbert => new_colbert,
+        :original_airdate => original_airdate
+      }
 end
